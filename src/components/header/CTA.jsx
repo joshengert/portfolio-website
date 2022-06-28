563ba339df8d989f2a2386c5cf87d74d0aee0948
Josh Engert <joshengert@hotmail.com>
2022-06-28 21:37:11 +1200

    fixed downloads

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/CV_JoshEngert.pdf'
+import CV from './CV_JoshEngert.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download="CV_JoshEngert.pdf" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>    
     </div>
   )
