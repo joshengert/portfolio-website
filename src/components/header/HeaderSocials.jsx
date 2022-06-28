16e130d59a66e6e06e56ca41b81d663124fac619
Josh Engert <joshengert@hotmail.com>
2022-06-28 20:52:01 +1200

    Initial commit

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com/in/joshengert" target="_blank" rel='noreferrer'><BsLinkedin /></a>
+        <a href="https://github.com/joshengert" target="_blank" rel='noreferrer'><FaGithub /></a>  
+    </div>
+  )
+}
+
+export default HeaderSocials
