c85761d55c96c0d8e71dbfe573a450ff8fb3459e
Josh Engert <joshengert@hotmail.com>
2022-09-19 19:02:10 +1200

    added about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/mixing.jpg'
+import {AiOutlineDesktop} from 'react-icons/ai'
+import {SiArduino} from 'react-icons/si'
+import {BsController} from 'react-icons/bs'
 
 const About = () => {
   return (
     <section id='about'>
       <h5>What You Need To Know</h5>
       <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <AiOutlineDesktop className='about__icon'/>
+              <h5>Desktop Development</h5>
+            </article>
+
+            <article className="about__card">
+              <SiArduino className='about__icon'/>
+              <h5>Embedded Systems</h5>
+            </article>
+
+            <article className="about__card">
+              <BsController className='about__icon'/>
+              <h5>Game Development</h5>
+            </article>
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore officiis fugiat, obcaecati expedita earum deleniti, numquam harum veniam, esse cum unde? Omnis harum corrupti doloribus temporibus eius facere repudiandae explicabo?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BiUserCircle} from 'react-icons/bi'
+import {MdComputer} from 'react-icons/md'
 import {AiOutlineBook} from 'react-icons/ai'
 import {BiMessageDetail} from 'react-icons/bi'
 import { useState } from 'react'
@@ -12,6 +13,7 @@ const Nav = () => {
     <nav>
       <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdComputer/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineBook/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
